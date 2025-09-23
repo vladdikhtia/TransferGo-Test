b2fa393e122cba4f49eac8f92b79882c60c34239
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2025-09-24 00:25:53 +0200

    Added assets

---
+++ TransferGo-Task/TransferGo-Task/ConverterView.swift
@@ -0,0 +1,18 @@
+//
+//  ConverterView.swift
+//  TransferGo-Task
+//
+//  Created by Vladyslav Dikhtiaruk on 23/09/2025.
+//
+
+import SwiftUI
+
+struct ConverterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConverterView()
+}
